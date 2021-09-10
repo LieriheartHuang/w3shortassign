e15a71c760fbdb94dc0e3f81ebfde956a2024399
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-09 21:35:28 -0700

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,6 +1,18 @@
 a=float(input('Enter CPU gigahertz:\n')
 
 b=int(input('Enter CPU core count:\n')
+     
+      
+if b>=20:
+      print('That is a high-performance CPU.')
+elif b>=8 and a>=3.2:
+      print('That is a high-performance CPU.')
+elif b>=6 and a>=2.5:
+      print('That is a medium-performance CPU.')
+elif b>=2 and a>=2.0
+      print('That is a low-performance CPU.')
+else:
+      print('That CPU could use an upgrade.')
 
 
 That is a low-performance CPU.
